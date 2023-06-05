8105363dd18092461264e2cf9e3729b9ee4b3faf
Muhamed Muminović <muminovic.muhamed01@gmail.com>
2023-06-05 20:37:21 +0200

    edit unlocked

--- src/routes/quotes.js
+++ src/routes/quotes.js
@@ -6,12 +6,15 @@ const quotesController = require('../controllers/quote')
 const isAuth = require('../middlewares/isAuth')
 
 router.get('/get-quote/:quoteId', quotesController.getQuote)
-router.get('/get-by-category/:categoryId', quotesController.getAllQuotesByCategory)
+router.get(
+    '/get-by-category/:categoryId',
+    quotesController.getAllQuotesByCategory
+)
 router.get('/get-all-quotes', quotesController.getAllQuotes)
 router.get('/get-random-quote', quotesController.getRandomQuote)
 router.post('/add-quote', isAuth, quotesController.addQuote)
 router.patch('/like/:quoteId', quotesController.likeQuote)
-router.patch('/edit/:quoteId', isAuth, quotesController.editQuote)
+router.patch('/edit/:quoteId', quotesController.editQuote)
 router.delete('/delete/:quoteId', isAuth, quotesController.deleteQuote)
 
 module.exports = router
